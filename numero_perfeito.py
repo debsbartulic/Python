1e045d78113eb26dea4527049b892a9cfb66c862
debsbartulic <debsbartulic@gmail.com>
2017-10-04 15:21:59 -0300

    Ok
    Validado na plataforma!

--- numero_perfeito.py
+++ numero_perfeito.py
@@ -1,16 +1,11 @@
+# Número perfeito
+# Desenvolva a função numero_perfeito que receberá como entrada um número e retornará True se ele for perfeito e False caso contrário.
 def numero_perfeito(num):
-    res = 0
-    total = 0
-    aux = num
-    while (res != 1):
-        if (aux % 2 == 0):
-            res = aux / 2
-            total = total + res
-        else:
-            res = aux / 2 + 1
-            total = total + res
-        aux = aux / 2
-    if (total == num):
-        return True
-    else:
-        return False
+	total = 0
+	for i in range(1, num):
+		if (num % i == 0):
+			total = total + i
+	if (total == num):
+		return True
+	else:
+		return False
