2f5d2ae08eb3ea388dbb6ac20c613c624a8622a1
debsbartulic <debsbartulic@gmail.com>
2017-10-04 15:29:43 -0300

    Create obter_mais_longa_substring.py

---
+++ Udemy/3/obter_mais_longa_substring.py
@@ -0,0 +1,9 @@
+# A mais longa substring em ordem alfabética
+# Esse problema é um pouquinho mais difícil, mas não se preocupe, você conseguirá resolvê-lo!
+# Você desenvolverá uma função chamada obter_mais_longa_substring onde receberá como entrada uma string e você terá que retornar a mais longa substring em ordem alfabética.
+# Exemplo de entrada: azcbobobegghakl
+# Exemplo de saída: beggh
+# Lembrando que substring é uma sequência de caracteres contíguos (adjacente) de uma string.
+# Por exemplo, se eu tenho a string python, a string pto NÃO é substring porque não são caracteres 
+
+def obter_mais_longa_substring(s):
