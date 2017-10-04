8aeb2887aa9a688863b196db3ee2b06cd668d42f
debsbartulic <debsbartulic@gmail.com>
2017-10-04 15:10:10 -0300

    OK
    Validada na plataforma.

--- tupla_par.py
+++ tupla_par.py
@@ -1,4 +1,13 @@
+# Tupla Par
+# Vamos resolver um exercício que envolve tuplas? Você deverá desenvolver uma função chamada tupla_par. 
+# Essa função recebe uma tupla como entrada e retorna uma nova tupla como saída onde os elementos dessa nova tupla são os elementos 
+# cujos índices são números pares. Lembrando que o Python indexa a partir do zero.
+# Exemplo de tupla de entrada: ('oi', 'estou', 'estudando', 'poo')
+# Tupla que deve ser retornada: ('oi', 'estudando') #
+
 def tupla_par(tupla):
-	for ponteiro in range(0,len(t)):
+	listSaida = []
+	for ponteiro in range(0,len(tupla)):
 		if (ponteiro % 2 == 0):
-			print(t[ponteiro])
+			listSaida.append(tupla[ponteiro])
+	return tuple(listSaida)
