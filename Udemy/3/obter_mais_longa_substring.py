2a85b7258de488d407e47e7bb8c720ce718552c5
debsbartulic <debsbartulic@gmail.com>
2017-10-04 17:23:10 -0300

    Update obter_mais_longa_substring.py

--- Udemy/3/obter_mais_longa_substring.py
+++ Udemy/3/obter_mais_longa_substring.py
@@ -7,6 +7,7 @@ def obter_mais_longa_substring(s):
 	d = { 'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26 }
 	listSaida = []
 	st = ''
+	maior = ''
 	for i in range(0, len(s)):
             st = st + s[i]
             for j in range(i+1, len(s)):
@@ -17,4 +18,8 @@ def obter_mais_longa_substring(s):
 			    listSaida.append(st)
 			    st = ''
 			    break;
-        print(listSaida)
+        for item in lista:
+		if (len(item) > len(maior)):
+			maior = item
+	print(maior)
+		
