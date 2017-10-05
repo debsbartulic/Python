ad48855bbc31a82b7e265282cbb97eea020b693c
debsbartulic <debsbartulic@gmail.com>
2017-10-05 11:36:58 -0300

    Create docstrings.py

---
+++ Udemy/3/Aula27/docstrings.py
@@ -0,0 +1,19 @@
+# docstring 
+# string inseridas dentro do código python para explicar o 
+# funcionamento do código
+
+def multiplica(x, y):
+	""" Essa função retorna a multiplicação de 2 números """
+	return x * y
+
+help(multiplica)
+
+multiplica.__doc__
+
+class MinhaClasse:
+	"""Classe utilizada para representar algo"""
+	pass
+
+help(MinhaClasse)
+
+MinhaClasse.__doc__
